f29c458dc2ad7083e0c0e95429160419589d63af
Joseph  Deen <josephdeenp.f@live.com>
2022-02-11 14:26:08 +0530

    error fix

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const authController = require('../controllers/auth.controller')
+const verifySignUp = require("../middleware/verify.signup")
 
 
 const router = express.Router();
